57ce5e8a109957279e5eefa3186f7cb362987c0e
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-01-05 09:48:11 +0200

    New todolist-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {TodoWrapperLocalStorage} from './components/TodoWrapperLocalStorage'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapperLocalStorage/>
+      
+    </div>
+  );
+}
+
+export default App;
